2f1d2ca2424e5e0742d979ae57feebd64cdaf494
GibbedHead <chaplygin-ma@yandex.ru>
2024-05-23 13:23:06 +0300

    Структура автоконфигуратора и класса свойств

---
+++ http-logging-spring-boot-starter/http-logging-autoconfigure/src/main/java/ru/chaplyginma/properties/HttpLoggingProperties.java
@@ -0,0 +1,13 @@
+package ru.chaplyginma.properties;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "http-logging")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class HttpLoggingProperties {
+    Boolean enabled;
+}
