f7ef7f7c88f7c46f3d8019b85b89e82a8e33183f
GibbedHead <chaplygin-ma@yandex.ru>
2024-05-23 12:21:06 +0300

    2 приложения для проверки стартера web и reactive web

---
+++ sample-reactive-web-app/src/test/java/ru/chaplyginma/samplereactivewebapp/SampleReactiveWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chaplyginma.samplereactivewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleReactiveWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ sample-web-app/src/test/java/ru/chaplyginma/samplewebapp/SampleWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chaplyginma.samplewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
