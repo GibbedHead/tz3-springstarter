801d95db6d0a2c2cce043938e8785a09c10feda0
GibbedHead <chaplygin-ma@yandex.ru>
2024-05-28 17:12:21 +0300

    Простые интеграционные тесты что фильтры работают при запросах

--- http-logging-spring-boot-starter-parent/http-logging-tests/src/test/java/ru/chaplyginma/httploggingspringbootstarter/integrationtests/ReactiveWebHttpLoggingFilterIntegrationTests.java
+++ http-logging-spring-boot-starter-parent/http-logging-tests/src/test/java/ru/chaplyginma/httploggingspringbootstarter/integrationtests/ReactiveWebHttpLoggingFilterIntegrationTests.java
@@ -1,7 +1,13 @@
 package ru.chaplyginma.httploggingspringbootstarter.integrationtests;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,14 +15,37 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import ru.chaplyginma.httploggingspringbootstarter.config.ReactiveWebHttpLoggingTestConfig;
+import ru.chaplyginma.httploggingspringbootstarter.logger.MemoryAppender;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootApplication
 @ExtendWith(SpringExtension.class)
 @Import(ReactiveWebHttpLoggingTestConfig.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ReactiveWebHttpLoggingFilterIntegrationTests {
+    private static final String LOGGER_NAME = "ru.chaplyginma.httplogging.logger.HttpLogger";
+    private static final String MSG = "Request:";
+    private static MemoryAppender memoryAppender;
     @Autowired
     private WebTestClient webTestClient;
+
+    @BeforeEach
+    public void setup() {
+        Logger logger = (Logger) LoggerFactory.getLogger(LOGGER_NAME);
+        memoryAppender = new MemoryAppender();
+        memoryAppender.setContext((LoggerContext) LoggerFactory.getILoggerFactory());
+        logger.setLevel(Level.DEBUG);
+        logger.addAppender(memoryAppender);
+        memoryAppender.start();
+
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        memoryAppender.reset();
+        memoryAppender.stop();
+    }
 
     @Test
     public void testLoggingFilter() {
@@ -25,6 +54,6 @@ public class ReactiveWebHttpLoggingFilterIntegrationTests {
                 .exchange()
                 .expectStatus().isNotFound();
 
-
+        assertThat(memoryAppender.search(MSG, Level.INFO).size()).isEqualTo(1);
     }
 }
--- http-logging-spring-boot-starter-parent/http-logging-tests/src/test/java/ru/chaplyginma/httploggingspringbootstarter/integrationtests/WebHttpLoggingFilterIntegrationTests.java
+++ http-logging-spring-boot-starter-parent/http-logging-tests/src/test/java/ru/chaplyginma/httploggingspringbootstarter/integrationtests/WebHttpLoggingFilterIntegrationTests.java
@@ -1,7 +1,13 @@
 package ru.chaplyginma.httploggingspringbootstarter.integrationtests;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +18,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import ru.chaplyginma.httploggingspringbootstarter.config.WebHttpLoggingTestConfig;
+import ru.chaplyginma.httploggingspringbootstarter.logger.MemoryAppender;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @Import(WebHttpLoggingTestConfig.class)
@@ -19,13 +28,36 @@ import ru.chaplyginma.httploggingspringbootstarter.config.WebHttpLoggingTestConf
 @AutoConfigureMockMvc
 public class WebHttpLoggingFilterIntegrationTests {
 
+    private static final String LOGGER_NAME = "ru.chaplyginma.httplogging.logger.HttpLogger";
+    private static final String MSG = "Request:";
+    private static MemoryAppender memoryAppender;
     @Autowired
     private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        Logger logger = (Logger) LoggerFactory.getLogger(LOGGER_NAME);
+        memoryAppender = new MemoryAppender();
+        memoryAppender.setContext((LoggerContext) LoggerFactory.getILoggerFactory());
+        logger.setLevel(Level.DEBUG);
+        logger.addAppender(memoryAppender);
+        memoryAppender.start();
+
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        memoryAppender.reset();
+        memoryAppender.stop();
+    }
+
 
     @Test
     public void testController() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/your-endpoint")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isNotFound());
+
+        assertThat(memoryAppender.search(MSG, Level.INFO).size()).isEqualTo(1);
     }
 }
